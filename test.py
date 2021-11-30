8b4f660330913106fddc8b5023cca7af577cf784
9Hundred100Five1 <69020075+9Hundred100Five1@users.noreply.github.com>
2021-11-30 19:45:25 +0900

    심심해서 만듬

---
+++ test.py
@@ -0,0 +1,18 @@
+import requests #dependency
+
+url = "https://discord.com/api/webhooks/909804938396323860/ERHlv2euuke_8GQHDyRb2zCDVcvdFUyWYd1pls3pN9LEtWfNVLlv86PXbK0sAisQ8F7p" #webhook url, from here: https://i.imgur.com/f9XnAew.png
+while True:
+    i = input()
+    data = {
+        "content" : i,
+        "username" : "test"
+    }
+
+    req = requests.post(url, json = data)
+
+    try:
+        req.raise_for_status()
+    except requests.exceptions.HTTPError as err:
+        print(err)
+    else:
+        print("전송완료, 코드 {}".format(req.status_code))
