9fab12fc88a4b3af367689d65c7107a720a1b142
9Hundred100Five1 <69020075+9Hundred100Five1@users.noreply.github.com>
2021-11-30 22:24:12 +0900

    Add files via upload

---
+++ simple-discordWebhook-sender/Send/send.py
@@ -0,0 +1,35 @@
+from typing import Any
+from asyncio.runners import run
+import requests
+import asyncio
+
+class sender:
+    def __init__(self, url:str, cont:str, usn:str):
+        self.url = url
+        self.cont = cont
+        self.usn = usn
+
+    async def send(self):
+        url = self.url
+        content = self.cont
+        username = self.usn
+        data = {
+            "content" : content,
+            "username" : username 
+        }
+
+        req = requests.post(url, json = data)
+
+        try:
+            req.raise_for_status()
+        except requests.exceptions.HTTPError as err:
+            return print(err)
+        else:
+            return print("전송완료, 코드 {}".format(req.status_code))
+
+    def sends(self):
+        asyncio.run(sender.send(self))
+
+    
+
+        
